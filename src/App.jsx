8a4a727f33f2b3d4e0d89a222750901c2251220f
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-03-12 16:16:56 -0300

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import TabelaIMC from "./componentes/Tabela/Index";
-import Formulario from "./componentes/Formulario/index"; // Adicionei a importação do componente Formulario
+import Formulario from "./componentes/Formulario/index";
 
 function App() {
   return (
