059b076902e7ef8224c6e41aa9185dff1425fa7e
Lorenzo Gutierrez <lorenzoplaypereira@gmail.com>
2024-03-12 16:03:16 -0300

    first commit

---
+++ src/componentes/Tabela/Index.jsx
@@ -0,0 +1,13 @@
+
+import "../Tabela/Tabela.modules.css";
+
+function TabelaIMC() {
+  return (
+    <div className="blocoTab">
+      <table className="table">
+      </table>
+    </div>
+  );
+}
+
+export default TabelaIMC;
